fe9b7b32483aa92b6e6da9a2196ede9c6ab8e606
Renato <renatoperezc@gmail.com>
2022-09-10 19:58:13 -0600

    Add installment utility func

---
+++ src/libs/installments.ts
@@ -0,0 +1,12 @@
+const round = (amountOfMoney: number): number => Math.round(amountOfMoney * 100) / 100;
+
+const monthlyPayment = (principal: number, annualInterestRate: number, payments: number = 12): number => {
+  const interest = annualInterestRate / 100 / 12;
+  const x = Math.pow(1 + interest, payments);
+  const monthly = (principal * x * interest) / (x - 1);
+  if (isNaN(monthly)) {
+    throw new Error("Error calculating monthly payment");
+  }
+  return round(monthly);
+};
+
